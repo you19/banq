4e5ab012166031a5be51e0e9002ee429600aacb1
you19 <34568972+you19@users.noreply.github.com>
2018-11-27 23:39:03 +0100

    Add files via upload

---
+++ MaBanque/src/main/java/com/example/demo/MaBanqueApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaBanqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaBanqueApplication.class, args);
+	}
+}
---
+++ MaBanque/src/main/java/com/example/entites/CompteCourant.java
@@ -0,0 +1,31 @@
+package com.example.entites;
+
+import java.sql.Date;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+@Entity
+@DiscriminatorValue("CC")
+public class CompteCourant extends Compte {
+private double decouvert;
+
+public CompteCourant() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+
+public CompteCourant(String codeCompte, Date dateCreation, double solde, Client client, double decouvert) {
+	super(codeCompte, dateCreation, solde, client);
+	this.decouvert = decouvert;
+}
+
+public double getDecouvert() {
+	return decouvert;
+}
+
+public void setDecouvert(double decouvert) {
+	this.decouvert = decouvert;
+}
+
+
+}
